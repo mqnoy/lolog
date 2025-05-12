mqnoy/lolog

commit b73b912b8249f3663f3dd4929fa7d00383ec473d

fixes log error function

src/lib/lolog.ts
@@ -51,16 +51,15 @@ export class Lolog implements LologContract {
     } as LogEntry);
   };
 
-  error(message: string | Error, meta?: Record<string, unknown>) {
+  error = (message?: string | Error, meta?: Record<string, unknown>) => {
     if (message instanceof Error) {
       this.log("error", message.message, {
         error: serializeError(message),
         ...meta,
       });
-    } else {
-      this.log("error", message, meta);
+      return;
     }
-  }
+  };
 
   warn(message: string, meta?: Record<string, unknown>) {
     this.log("warn", message, meta);

commit 801e5cbd383e69462d243cb4c0b812a213bf6d34

fixes b73b912 accidentally commited

src/lib/lolog.ts
@@ -51,15 +51,16 @@ export class Lolog implements LologContract {
     } as LogEntry);
   };
 
-  error = (message?: string | Error, meta?: Record<string, unknown>) => {
+  error(message: string | Error, meta?: Record<string, unknown>) {
     if (message instanceof Error) {
       this.log("error", message.message, {
         error: serializeError(message),
         ...meta,
       });
-      return;
+    } else {
+      this.log("error", message, meta);
     }
-  };
+  }
 
   warn(message: string, meta?: Record<string, unknown>) {
     this.log("warn", message, meta);